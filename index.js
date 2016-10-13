ef2cdc05ee2facdf17a5ef488c6f4e80e79f4d41
AVANADE-CORP\m.bueno.de.campos <sun.marcelo@gamil.com>
2016-10-13 09:44:17 -0300

    Estamos modificando o arquivo para adicionar o log

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var daapp = require('./daapp/server.js');
 var daweb = require('./daweb/bin/www');
 var digitalagris = require('./digitalagris/bin/www');
 
-
+console.log("Servidores estão sendo inicializados");
